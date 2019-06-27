8c31be49d4f020c1b2a0e944e3d96da865acbb37
unknown <gabethiesens@gmail.com>
2019-06-27 19:25:05 -0300

    terminei aula 2 modulo 3

---
+++ modulo3/aulas/aula2/main.js
@@ -0,0 +1,11 @@
+var listElement =  document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var ButtonElement = document.querySelector("#app button");
+
+var todos = [
+    "fazer cafe",
+    "estudar",
+    "acessar comunidade"
+];
+
+//arrey pq eh soh texto
